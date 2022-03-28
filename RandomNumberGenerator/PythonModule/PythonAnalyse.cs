2f7ba4ac6ea817f270b8d99d2aa95268f8bef51a
cheeze <maksprofy51@gmail.com>
2022-03-28 20:54:51 +0300

    Task1: refactoring

--- RandomNumberGenerator/PythonModule/PythonAnalyse.cs
+++ RandomNumberGenerator/PythonModule/PythonAnalyse.cs
@@ -9,10 +9,15 @@ namespace RandomNumberGenerator.PythonModule
         public PythonAnalyser()
         {
             _engine = Python.CreateEngine();
+            ICollection<string> searchPaths = _engine.GetSearchPaths();
+            searchPaths.Add(@"./PythonModule/venv/lib");
+            searchPaths.Add(@"./PythonModule/venv/lib/site-packages");
+            _engine.SetSearchPaths(searchPaths);
         }
 
         public void Analyse()
-        { _engine.ExecuteFile(@"./test.py");
+        { 
+            _engine.ExecuteFile(@"./PythonModule/analyser.py");
         }
     }
 }
